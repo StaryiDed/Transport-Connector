359bb7ecf88158a181f73d7290ade7bd9e418010
StaryiDed <55537598+StaryiDed@users.noreply.github.com>
2024-01-19 00:31:22 +0100

    Update connector.py

--- connector.py
+++ connector.py
@@ -1,64 +1,96 @@
 import asyncio
-from abc import ABC, abstractmethod
+import discord
+from discord.ext import commands
+from telegram import Update
+from telegram.ext import Updater, MessageHandler, Filters, CallbackContext
 
-class ChatTransport(ABC):
+
+class ChatTransport:
     def __init__(self):
-        self.handlers = {}
+        self.handlers = []
 
-    def add_handler(self, event, handler):
-        self.handlers[event] = handler
+    def add_handler(self, event):
+        self.handlers.append(event)
 
-    @abstractmethod
     async def send_message(self, msg):
-        pass
+        print(f"Sending message: {msg}")
 
-    @abstractmethod
     async def run(self):
-        pass
+        raise NotImplementedError("run method must be implemented in the subclass")
+
 
 class ChatTransportDiscord(ChatTransport):
-    def __init__(self, token, channel_id):
+    def __init__(self, bot_token):
         super().__init__()
-        import discord
-        self.client = discord.Client()
-        self.token = token
-        self.channel_id = channel_id
-
-        @self.client.event
-        async def on_ready():
-            print('Discord bot is ready.')
-
-        @self.client.event
-        async def on_message(message):
-            if message.author == self.client.user or message.channel.id != self.channel_id:
-                return
-            if 'message' in self.handlers:
-                await self.handlers['message'](message.content)
+        intents = discord.Intents.all()
+        self.bot = commands.Bot(command_prefix='!', intents=intents)
+        self.token = bot_token
 
     async def send_message(self, msg):
-        channel = self.client.get_channel(self.channel_id)
-        if channel:
-            await channel.send(msg)
+        for handler in self.handlers:
+            await handler(msg)
 
     async def run(self):
-        await self.client.start(self.token)
+        @self.bot.event
+        async def on_ready():
+            print(f'Discord Bot logged in as {self.bot.user.name}')
+
+        @self.bot.event
+        async def on_message(message):
+            await self.send_message(message.content)
+
+        await self.bot.start(self.token)
+
 
 class ChatTransportTelegram(ChatTransport):
-    def __init__(self, token):
+    def __init__(self, bot_token):
         super().__init__()
-        from telegram.ext import Updater, MessageHandler, Filters
-        self.updater = Updater(token, use_context=True)
-
-        def handle_message(update, context):
-            if 'message' in self.handlers:
-                asyncio.run(self.handlers['message'](update.message.text))
-
-        self.updater.dispatcher.add_handler(MessageHandler(Filters.text, handle_message))
+        self.updater = Updater(bot_token, use_context=True)
+        self.dp = self.updater.dispatcher
 
     async def send_message(self, msg):
-        for chat_id in self.updater.dispatcher.chat_data.keys():
-            self.updater.bot.send_message(chat_id, msg)
+        for handler in self.handlers:
+            await handler(msg)
 
     async def run(self):
+        def handle_telegram_message(update: Update, context: CallbackContext):
+            msg = update.message.text
+            asyncio.create_task(self.send_message(msg))
+
+        message_handler = MessageHandler(Filters.text & ~Filters.command, handle_telegram_message)
+        self.dp.add_handler(message_handler)
+
         self.updater.start_polling()
-        await asyncio.Event().wait()
+        self.updater.idle()
+
+
+class SimpleBusinessLogicBot:
+    def __init__(self, chat_transport):
+        self.chat_transport = chat_transport
+        self.chat_transport.add_handler(self.handle_message)
+
+    async def handle_message(self, msg):
+        response = f"Hi! Your message was received: {msg}"
+        await self.chat_transport.send_message(response)
+
+    async def start(self):
+        await self.chat_transport.run()
+
+
+discord_token = 'Discord Token'
+discord_transport = ChatTransportDiscord(discord_token)
+bot_discord = SimpleBusinessLogicBot(discord_transport)
+
+telegram_token = 'Telegram Token'
+telegram_transport = ChatTransportTelegram(telegram_token)
+bot_telegram = SimpleBusinessLogicBot(telegram_transport)
+
+
+async def run_bots():
+    await asyncio.gather(
+        bot_discord.start(),
+        bot_telegram.start()
+    )
+
+
+asyncio.run(run_bots())
